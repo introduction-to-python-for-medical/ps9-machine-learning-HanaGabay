9595de8c50fc445b142e2c50f2ed305b27ae770a
HanaGabay <chanag053@gmail.com>
2025-01-08 19:13:17 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,21 @@
-
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
-%load_ext autoreload
-%autoreload 2
-
-# Download the data from your GitHub repository
-!wget https://raw.githubusercontent.com/yotam-biu/ps9/main/parkinsons.csv -O /content/parkinsons.csv
-
 df = pd.read_csv('parkinsons.csv')
-
-features = ['PPE', 'RPDE','DFA']
+features = ['HNR', 'DFA']
 target = 'status'
 x = df[features]
 y = df[target]
+from sklearn.preprocessing import MinMaxScaler
 
 scaler = MinMaxScaler()
 x = scaler.fit_transform(x)
-
-
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42) # Example test_size and random_state
-
-
-svm_model = SVC(kernel='linear') # You can change the kernel (e.g., 'rbf', 'poly')
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+from sklearn.svm import SVC
+svm_model = SVC(kernel='linear') 
 svm_model.fit(x_train, y_train)
 
 
 y_pred = svm_model.predict(x_test)
-
-
+from sklearn.metrics import accuracy_score
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy}")
