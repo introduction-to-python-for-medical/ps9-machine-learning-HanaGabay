1d3d615439a77228dfec05c3415befec5f269ade
HanaGabay <chanag053@gmail.com>
2025-01-09 09:20:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,22 +4,20 @@ features = ['DFA', 'PPE']
 target = 'status'
 x = df[features]
 y = df[target]
-from sklearn.preprocessing import MinMaxScaler
 
+from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
 x = scaler.fit_transform(x)
+
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
 from sklearn.svm import SVC
 svm_model = SVC(kernel='linear') 
 svm_model.fit(x_train, y_train)
-
-
 y_pred = svm_model.predict(x_test)
+
 from sklearn.metrics import accuracy_score
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {accuracy}")
 
-import joblib
-
-joblib.dump(svm_model, 'my_model.joblib')
